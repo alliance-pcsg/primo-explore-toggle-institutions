da0ef2bc73b06425e5cef9b23cd0d0ee3a29ddc1
Mike Flakus <mflakus@pdx.edu>
2017-03-03 15:20:52 -0800

    removed references to summit

--- custom.js
+++ custom.js
@@ -9,25 +9,28 @@
     /*var app = angular.module('centralCustom', ['angularLoad']);*/
     /****************************************************************************************************/
 	
-	// Summit Hide/Show Button
+	/* Hide/Show Button */
 	app.component('prmAlmaMoreInstAfter', {
 		 bindings: {parentCtrl: '<'},
-		 template: '<div><hr />   <button class="hide_show_summit_libraries_button" onclick="hide_show_summit_libraries()" something>Show Summit Libraries</button></div>'
+		 template: '<div><hr />   <button class="hide_show_other_institutions_button" onclick="hide_show_other_institutions()" something>Show Libraries</button></div>'
 	});
 
 })();
 
 
-function hide_show_summit_libraries() 
+function hide_show_other_institutions() 
 {
 	if(angular.element(document.querySelector('md-tabs')).hasClass("display-block-important"))
 	{
 		angular.element(document.querySelector('md-tabs')).removeClass("display-block-important");
-		angular.element(document.getElementsByClassName('hide_show_summit_libraries_button')).text("Show Summit Libraries");
+		angular.element(document.getElementsByClassName('hide_show_other_institutions_button')).text("Show Libraries");
 	}
 	else
 	{
 		angular.element(document.querySelector('md-tabs')).addClass("display-block-important");
-		angular.element(document.getElementsByClassName('hide_show_summit_libraries_button')).text("Hide Summit Libraries");
+		angular.element(document.getElementsByClassName('hide_show_other_institutions_button')).text("Hide Libraries");
 	}
+	
+	// place button above list of libraries 
+	angular.element(document.querySelector('prm-alma-more-inst-after')).after(angular.element(document.querySelector('prm-alma-more-inst md-tabs')));
 }
